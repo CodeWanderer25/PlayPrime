8c9ab4d384736d8039dee5272febf9464ae96f71
CodeWanderer25 <priyanshucode2025@gmail.com>
2025-03-14 23:00:28 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDhkZTBkZjRmNzI5MmNkMDE3ODZlOTc2MjU0YjU0OCIsIm5iZiI6MTczOTU5MzEwMS44NjUsInN1YiI6IjY3YjAxNThkNDdkYmZiYTQ2NDZjNDQ2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I6xR7JSJAUznaeGSPWYZ_Po0C7h4mCqus0HYzScQxD0'
+        Authorization: 'Bearer YOUR_API_KEY'
       },
 
 });
