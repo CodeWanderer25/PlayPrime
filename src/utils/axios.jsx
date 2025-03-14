23318a5e1e1b89ea1c2e1cea9049180a5afc9078
CodeWanderer25 <priyanshucode2025@gmail.com>
2025-03-14 23:11:16 +0530

    modified

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,12 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDhkZTBkZjRmNzI5MmNkMDE3ODZlOTc2MjU0YjU0OCIsIm5iZiI6MTczOTU5MzEwMS44NjUsInN1YiI6IjY3YjAxNThkNDdkYmZiYTQ2NDZjNDQ2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I6xR7JSJAUznaeGSPWYZ_Po0C7h4mCqus0HYzScQxD0'
-      },
-
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+  },
 });
 
 export default instance;
